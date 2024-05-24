b49d7a81eb0339153943f6f67c8e0d3229938e7c
Bonthu Srinivasu <130753139+srinivasbonthu551@users.noreply.github.com>
2024-05-23 21:01:39 -0700

    Add files via upload

--- Abstraction.py
+++ Abstraction.py
@@ -1,13 +1,24 @@
 from abc import ABC, abstractmethod
 
+# Define an abstract class
 class Abstractdemo(ABC):
    @abstractmethod
    def car(self):
-        print("AUDI")
+        pass  # Abstract method should not have an implementation
+
+# Define a concrete subclass
 class Demo(Abstractdemo):
     def car(self):
         print("PULSAR")
-demo1=Demo()
-print(demo1.car())
-demo2=Abstractdemo()
-print(demo2.car())
+
+# Instantiate the concrete class and call the method
+demo1 = Demo()
+demo1.car()  # This will print "PULSAR"
+
+# Attempting to instantiate the abstract class will raise an error
+try:
+    demo2 = Abstractdemo()
+    demo2.car()
+except TypeError as e:
+    print(e)  # This will print an error message
+
